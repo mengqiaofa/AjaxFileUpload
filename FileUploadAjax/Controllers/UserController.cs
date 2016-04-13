b01415f70a680b13ca272f17020acd0dc7acb243
saifullah <saifullah.iqbal@tenpearls.com>
2016-04-13 11:10:02 +0500

    Fileuploader ajax. UserModel.cs added.

---
+++ FileUploadAjax/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using FileUploadAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadAjax.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(model.FileBase64))
+                {
+                    byte[] buffer = Convert.FromBase64String(model.FileBase64);
+                    string filename = model.FileName;
+                    /****************upload file*************/
+                }
+                return Json(new { Success = true });
+            }
+            return Json(new { Success = false});
+        }
+	}
+}
---
+++ FileUploadAjax/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FileUploadAjax.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public string FileName { get; set; }
+        public string FileBase64 { get; set; }
+    }
+}
